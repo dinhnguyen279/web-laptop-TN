c0444313a8a81a828b4bd3ab4028a10716321490
Thuận <88768977+TDT1401@users.noreply.github.com>
2022-09-14 13:33:31 +0700

    footer#1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <div className="text-red-500 text-2xl"> Laptop</div>;
+  return (
+    <div className="app">
+      <h1>header</h1>
+      <h1>body</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
