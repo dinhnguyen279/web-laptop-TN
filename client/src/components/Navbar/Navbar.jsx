8b02dcbe681fb69c33e4f51b489cc34af876c750
Nguyễn <tdinhnguyen279@gmail.com>
2022-09-15 13:39:46 +0700

    style: :art: format code

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -19,22 +19,19 @@ export const Navbar = () => {
   const showNavbar = () => {
     navRef.current.classList.toggle('responsive_nav');
   }
-
   return (
     <>
-      <div className=" h-full w-full bg-white">
-        <div className=" flex justify-center  py-3 container space-x-10">
+      <div className="h-full max-w-full bg-white">
+        <div className=" flex justify-center items-center container py-3 space-x-10">
           <div className="logo">
             <a href="/">
               <img className="w-full h-14 object-fill" src={logo} alt="logo" />
             </a>
           </div>
-          <div className="flex items-center space-x-5">
+          <div className="flex items-center justify-center space-x-5">
             <form className="">
               <div className="relative flex">
-                <div className="">
-                  <input className="border bg-gray-200 rounded-xl h-8 outline-none pl-3 w-48 sm:w-72 lg:w-72" type="text" placeholder="Tìm kiếm" />
-                </div>
+                  <input className="border bg-gray-200 rounded-xl h-8 outline-none pl-3 w-32 sm:w-48 xl:w-72" type="text" placeholder="Tìm kiếm" />
                 <div className=" absolute top-0 right-0">
                   <button className="bg-[#45aece] py-1 px-2 rounded-r-xl w-full text-[#a8e3f3]">
                     <FontAwesomeIcon icon={faSearch}  />
@@ -43,32 +40,32 @@ export const Navbar = () => {
               </div>
             </form>
             <nav className="nav-main">
-              <ul className="flex justify-center items-center space-x-3 text-center "ref={navRef}>
+              <ul className="flex justify-center space-x-3 text-center"ref={navRef}>
                 {/* text-[#45aece] */}
-                <button className="nav-btn .nav-close-btn" onClick={showNavbar}>
+                <button className="nav-btn px-3 py-0" onClick={showNavbar}>
                 <FontAwesomeIcon icon={faTimes} />
               </button>
-                <li className="">
+                <li>
                   <span><FontAwesomeIcon icon={faNotesMedical} className="icon" /> </span>
                   <p>Đăng ký</p>
                 </li>
-                <li className="">
+                <li>
                   <span><FontAwesomeIcon icon={faUser} className="icon"/></span>
                   <p>Đăng Nhập</p>
                 </li>
-                <li className="">
+                <li>
                   <span><FontAwesomeIcon icon={faCartArrowDown} className="icon"/></span>
                   <p>Giỏ hàng</p>
                 </li>
-                <li className="">
+                <li>
                   <span><FontAwesomeIcon icon={faPhoneSquare} className="icon"/></span>
                   <p>Tổng đài</p>
                 </li>
-                <li className="">
+                <li>
                   <span><FontAwesomeIcon icon={faVideoCamera} className="icon"/></span>
                   <p>Videos</p>
                 </li>
-                <li className="">
+                <li>
                   <span><FontAwesomeIcon icon={faRss}className="icon" /></span>
                   <p>Tin Công Nghệ</p>
                 </li>
@@ -77,7 +74,7 @@ export const Navbar = () => {
                   <p>Khuyến mãi</p> 
                 </li>
               </ul>
-              <button className="nav-btn lg:hidden flex" onClick={showNavbar}>
+              <button className="nav-btn flex lg:hidden" onClick={showNavbar}>
                 <FontAwesomeIcon icon={faBars} />
               </button>
             </nav>
