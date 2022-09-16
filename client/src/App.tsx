dfa5e21f293c739a1ac59a2f075559d6015adc88
Nguyễn <tdinhnguyen279@gmail.com>
2022-09-16 14:59:36 +0700

    docs: :memo: Fix document code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Footer from "./components/footer/Footer";
-import { Carousel } from "./components/Carousel/Carousel";
-import { Navbar } from "./components/Navbar/Navbar";
+import Carousel from "./components/Carousel/Carousel";
+import Navbar from "./components/Navbar/Navbar";
 import { dataCarousel } from "./components/Carousel/dataCarousel";
+import BestSeller from "./components/Products/BestSeller";
+
 function App() {
   return (
     <>
       <Navbar />
       <Carousel slides={dataCarousel} />
+      <BestSeller />
       <Footer />
     </>
   );
--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,40 +1,35 @@
 import React, { useState } from 'react'
-// import  Carousel1  from '../../assets/images/Carousel1.jpg';
-// import  Carousel2  from '../../assets/images/Carousel2.jpg';
-// import  Carousel3  from '../../assets/images/Carousel3.jpg';
-// import  Carousel4  from '../../assets/images/Carousel4.jpg';
-import '../Carousel/index.css'
 import { dataCarousel } from './dataCarousel'
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faArrowAltCircleLeft, faArrowAltCircleRight} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
 
-export const Carousel = ({slides}) => {
+const Carousel = ({ slides }) => {
     const [current, setCurrent] = useState(0)
     const length = slides.length
     const nextSlide = () => {
-        setCurrent(current === length -1 ? 0 : current + 1)
+        setCurrent(current === length - 1 ? 0 : current + 1)
     }
     const prevSlide = () => {
         setCurrent(current === 0 ? length - 1 : current - 1)
-        
     }
-    console.log(current);
     if (!Array.isArray(slides) || slides.length <= 0) {
         return null
     }
-  return (
-      <section className='relative h-[100vh] w-full flex justify-center items-start '>
-              <FontAwesomeIcon icon={faArrowAltCircleLeft} className="left-arrow text-white border-2 rounded-full hover:bg-[#45aece] absolute top-[10%] md:top-[25%] left-[10%] md:left-[20%] text-5xl z-10 cursor-pointer select-none " onClick={()=>prevSlide()}/>
-              <FontAwesomeIcon icon={faArrowAltCircleRight} className="right-arrow text-white border-2 rounded-full hover:bg-[#45aece] absolute top-[10%] md:top-[25%] right-[10%] md:right-[20%] text-5xl z-10 cursor-pointer select-none" onClick={()=>nextSlide()}/>
-              {dataCarousel.map((arr, idx) => {
-                  return (
-                      <div key={idx} className={idx === current ? ' relative h-[100vh] flex justify-center items-start opacity-100 duration-1000' : 'relative h-[100vh] flex justify-center items-start opacity-0 duration-1000 ease-in-out'}>
-                          {idx === current && (
-                              <img src={arr.image} alt="images" className="w-[100vw] h-[30vh] md:w-[60vw] md:h-[60vh] rounded-lg" />
-                          )}
-                      </div>
-                  )
-              })}
-      </section>
-  )
+    return (
+        <section className='relative m-auto h-full w-full flex items-start '>
+            <FontAwesomeIcon icon={faArrowAltCircleLeft} className="left-arrow text-white border-2 rounded-full hover:bg-[#45aece] absolute top-[10%] md:top-[20%] lg:top-[30%]  left-[10%] md:left-[5%] text-5xl z-10 cursor-pointer select-none " onClick={() => prevSlide()} />
+            <FontAwesomeIcon icon={faArrowAltCircleRight} className="right-arrow text-white border-2 rounded-full hover:bg-[#45aece] absolute top-[10%] md:top-[20%] lg:top-[30%]  right-[10%] md:right-[5%] text-5xl z-10 cursor-pointer select-none" onClick={() => nextSlide()} />
+            {dataCarousel.map((arr, idx) => {
+                return (
+                    <div key={idx} className={idx === current ? 'relative flex h-[50%] justify-center items-start opacity-100 duration-1000' : 'relative h-[100vh] flex justify-center items-start opacity-0 duration-1000 ease-in-out'}>
+                        {idx === current && (
+                            <img src={arr.image} alt="images" className="w-[100vw] h-[30vh] md:h-[40vh] lg:h-[70vh] rounded-lg" />
+                        )}
+                    </div>
+                )
+            })}
+            <a href='#pro' className='absolute opacity-90 top-[20%] left-[20%] md:top-[30%] lg:top-[50%] uppercase font-medium border-2 hover:bg-[#45aece] hover:text-white hover:no-underline text-[#45aece]  px-4 py-2 md:text-3xl'>Tìm hiểu thêm</a>
+        </section>
+    )
 }
+export default Carousel;
