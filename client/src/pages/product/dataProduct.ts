9ff66ed5428bc6929ccc37d239feab37ea2c56b5
Thuận <88768977+TDT1401@users.noreply.github.com>
2022-09-18 14:06:34 +0700

    product

---
+++ client/src/pages/product/dataProduct.ts
@@ -0,0 +1,104 @@
+export const Images = [
+  "https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png",
+  "https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png",
+  "https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png",
+  "https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png",
+
+];
+export const NameSpecifications = [
+  {
+    name: "Laptop gaming ASUS TUF Dash F15 FX517ZE HN888W",
+  },
+];
+export const Specifications = [
+  {
+    title: "CPU",
+    infor:
+      "Intel Core i7-12650H 2.3 GHz (24M Cache, up to 4.7 GHz, 6 P-cores and 4 E-cores)",
+  },
+  {
+    title: "RAM",
+    infor: "8GB DDR5 4800MHz (2 x SO-DIMM, tối đa 32GB)",
+  },
+  {
+    title: "Ổ CỨNG",
+    infor: "512GB SSD M.2 NVMe PCIe 3.0 (Còn trống 1 khe SSD M.2 PCIE)",
+  },
+  {
+    title: "CPU",
+    infor:
+      "Intel Core i7-12650H 2.3 GHz (24M Cache, up to 4.7 GHz, 6 P-cores and 4 E-cores)",
+  },
+  {
+    title: "Card đồ họa",
+    infor:
+      "NVIDIA GeForce RTX 3050Ti 4GB Up to 1535MHz at 75W (1485MHz Boost Clock+50MHz OC, 60W+15W Dynamic Boost)",
+  },
+  {
+    title: "Màn hình",
+    infor:
+      "15.6 inch FHD (1920 x 1080) 144Hz, IPS-level, anti-glare, sRGB:62.5%, Adobe:47.34%, Adaptive-Sync, MUX Switch + Optimus",
+  },
+  {
+    title: "Bàn phím",
+    infor: "Backlit Chiclet Keyboard",
+  },
+  {
+    title: "Chuẩn LAN",
+    infor: "10/100/1000 Mbps",
+  },
+  {
+    title: "Chuẩn WIFI",
+    infor: "Wi-Fi 6(802.11ax)",
+  },
+  {
+    title: "Bluetooth",
+    infor: "Bluetooth V5.2",
+  },
+  {
+    title: "Webcam",
+    infor: "720P HD camera",
+  },
+  {
+    title: "Hệ điều hành",
+    infor: "Windows 11 Home",
+  },
+  {
+    title: "Pin",
+    infor: "4 Cell 76Whr",
+  },
+  {
+    title: "Trọng lượng",
+    infor: "2.0 kg",
+  },
+  {
+    title: "Màu sắc",
+    infor: "Off Black",
+  },
+  {
+    title: "Kích thước",
+    infor: "35.4 x 25.1 x 1.99 cm",
+  },
+];
+export const DetailedReviews = [
+    {
+        'review': 'Đánh giá chi tiết Laptop gaming ASUS TUF Dash F15 FX517ZE-HN888W',
+        'detaile': 'Nếu Asus Vivobook và Asus Zenbook thống lĩnh thị trường laptop văn phòng, mỏng nhẹ thì phân khúc laptop gaming ASUS TUF Gaming chính là một cái tên đáng gờm. Tiếp nối sự thành công của những thế hệ trước - ASUS TUF Dash F15 FX517ZE-HN888W sẽ là một cái tên đầy triển vọng và hứa hẹn gây sốt cho cộng đồng game thủ thời gian sắp tới.',
+        'urlImages': 'https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png'
+    },
+    {
+        'review': 'Thiết kế hiện đại',
+        'detaile': 'Sở hữu ngôn ngữ thiết kế hiện đại với tông màu xám đen mạnh mẽ, cuốn hút, ASUS TUF Dash F15 FX517ZE-HN888W là sự trung hòa hoàn hảo giữa sự hầm hố, bụ bẫm vốn có của dòng laptop chơi game với sự trẻ trung, thanh lịch phù hợp với newbie cũng như những game thủ chuyên nghiệp.',
+        'urlImages': 'https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png'
+    },
+    {
+        'review': 'Hiệu năng ASUS TUF Dash F15 mạnh mẽ, ấn tượng',
+        'detaile': 'Hầu hết laptop gaming đều được trang bị sức mạnh khủng, ASUS TUF Dash F15 cũng không ngoại lệ. Bên trong là bộ vi xử lý Intel Core i7 thế hệ 12 mới nhất kết hợp với 8GB RAM DDR5 giúp máy đạt tốc độ xử lý cực nhanh. Card đồ họa rời NVIDIA GeForce RTX 3050Ti cho khả năng xử lý đồ họa và tái tạo hình ảnh mượt mà, bạn có thể chiêm ngưỡng mọi thước phim, từng chi tiết trong game một cách chi tiết và chân thật nhất. <br/> Bên cạnh đó, ổ cứng SSD dung lượng 512GB cho phép bạn lưu trữ một khối lượng lớn dữ liệu và ứng dụng. Nếu như bạn cảm thấy nó chưa đủ để phục vụ nhu cầu sử dụng của bạn thì ASUS TUF Dash F15 còn trống một khe SSD M.2 PCI-e để bạn có thể thoải mái nâng cấp khả năng lưu trữ trong tương lai.',
+        'urlImages': 'https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png'
+    },
+    {
+        'review': 'Độ bền đạt tiêu chuẩn quân đội',
+        'detaile': 'Hầu như các sản phẩm của nhà Asus từ màn hình PC đến laptop đều đạt độ bền chuẩn quân đội. Để xứng đáng với tên gọi TUF Gaming, chiếc laptop này phải vượt qua bài kiểm tra pin về độ bền chuẩn quân đội MIL-STD-810H. Các bài kiểm tra cho thiết bị bao gồm thả rơi và va đập, độ ẩm cao và nhiệt độ cực đoan nhằm đảm bảo độ laptop có thể vận hành ổn định ngay cả trong những môi trường khắc nghiệt nhất.',
+        'urlImages': 'https://product.hstatic.net/1000026716/product/hn888w_5dd5c18b72e54f1083a8f1a37ad5bb81.png'
+    },
+]
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -2,11 +2,14 @@ import Home from "../pages/home/home";
 import Cart from "../pages/cart/cart";
 import Store from "../pages/store/Store";
 import Pay from "../pages/pay/Pay";
+import Product from "../pages/product/Product";
+
 export const publicRouter = [
   { path: "/", component: Home },
   { path: "/cart", component: Cart },
   { path: "/store", component: Store },
   { path: "/pay", component: Pay },
+  { path: "/product", component: Product },
 ];
 
 export const privateRouter = [];
