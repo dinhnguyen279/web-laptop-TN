6c6f8a468be7962195594ee15b5eecbc0b15734f
Nguyễn <tdinhnguyen279@gmail.com>
2022-09-20 00:31:03 +0700

    refactor: :art: add Contact

--- client/src/components/Products/Contact/Contact.jsx
+++ client/src/components/Products/Contact/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = () => {
                 TƯ VẤN MIỄN PHÍ
             </h1>
             <hr className="border-black border-2 m-auto w-14" />
-            <div className='flex justify-center flex-col lg:flex-row container max-w-full'>
-                <form className=' p-4'>
-                    <div className=' text-gray-400 flex flex-col lg:flex-row w-full lg:space-x-2 space-x-0 '>
+            <div className='flex justify-center flex-col lg:flex-row container max-w-full '>
+                <form className='p-4'>
+                    <div className=' text-gray-400 flex flex-col lg:flex-row w-full lg:space-x-2 space-x-0 lg:space-y-0 space-y-2 mb-2'>
                         <div className='bg-[#d5dde9] h-9 border-[1px] rounded-3xl border-[#2a54a4] px-2 outline-none' >
                             <input type="text" name="Họ và Tên" id="" placeholder='Họ và Tên' className='bg-transparent h-9 px-2 border-none outline-none w-full' />
                         </div>
@@ -31,7 +31,7 @@ const Contact = () => {
                     </div>
                 </form>
                 <div className='flex flex-col p-4'>
-                    <p className='mb-3'>
+                    <p className='mb-3 text-lg'>
                         Bạn cần tư vấn về dòng laptop nào, hãy đặt câu hỏi với chuyên viên của chúng tôi.
                     </p>
                     <a href='tel:0829954124' target='_self' className='hover:no-underline text-center border-2 outline-none bg-[#2a54a4] rounded-3xl font-black uppercase px-3 py-2 text-white '><FontAwesomeIcon icon={faPhone} /> LIÊN HỆ CHÚNG TÔI</a>
